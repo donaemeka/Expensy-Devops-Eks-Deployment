b952dc80e65d0da7ccb4523a9b46af7273a1f9af
Donatus <donaemeka92@gmail.com>
2026-04-02 21:16:25 +0200

    Deploy Expensy on EKS with Kubernetes, Secrets, and production frontend

--- expensy_frontend/api/apiclient.tsx
+++ expensy_frontend/api/apiclient.tsx
@@ -1,23 +1,19 @@
 'use client'
 import axios from 'axios';
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
-// BASE_URL= "http://localhost:8080";
-console.log('BASE_URL', BASE_URL);
+
 export const apiClient = axios.create({
-  baseURL: BASE_URL,
-  headers: {    
+  headers: {
     'Content-Type': 'application/json',
   },
 });
 
 apiClient.interceptors.request.use(async config => {
-    const apiKey= process.env.NEXT_APP_API_KEY||'fergdhkdskgldkdd';
- 
+  const apiKey = process.env.NEXT_APP_API_KEY || 'fergdhkdskgldkdd';
+
   if (apiKey) {
     config.headers.Authorization = `Bearer ${apiKey}`;
   }
   return config;
-}
-);
+});
 
 export default apiClient;
